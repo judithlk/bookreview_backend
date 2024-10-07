a847f76e1b878c708601aaba022868ad892fc095
Judith <104207025+judithlk@users.noreply.github.com>
2024-10-07 21:36:54 +0100

    remove slash

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://rivu.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://rivu.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
