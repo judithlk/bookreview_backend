e78667d63aaf8130e38940668d09a81a9d51a1ff
Judith <104207025+judithlk@users.noreply.github.com>
2024-10-07 18:06:44 +0100

    corsheader

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({ origin: "https://bookreview-backend-3pg5.onrender.com/api/", methods: "GET,POST,PUT,DELETE", credentials: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Optional: if credentials are needed
+  next();
+});
+
+// app.use(cors({ origin: "https://bookreview-backend-3pg5.onrender.com/api/", methods: "GET,POST,PUT,DELETE", credentials: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
