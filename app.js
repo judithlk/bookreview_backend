b7cf226c043a5dfa44e66517403f3a5d666ac131
Judith <104207025+judithlk@users.noreply.github.com>
2024-10-08 12:58:05 +0100

    Updates

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 const bookRoutes = require("./routes/api/books");
 const reviewRoutes = require("./routes/api/reviews");
-const genreRoutes = require("./routes/api/genres")
+const genreRoutes = require("./routes/api/genres");
 const authRoutes = require("./routes/api/auth");
 const userRoutes = require("./routes/api/users");
-const searchRoutes = require("./routes/api/search")
+const searchRoutes = require("./routes/api/search");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use((req, res, next) => {
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Origin", "https://rivu.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
+// app.use(cors({ origin: "https://bookreview-backend-3pg5.onrender.com/api/", methods: "GET,POST,PUT,DELETE", credentials: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,9 +33,8 @@ app.use("/api/search", searchRoutes);
 
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello beautiful world!'));
+app.get("/", (req, res) => res.send("Hello beautiful world!"));
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-// app.use(cors({ origin: "https://bookreview-backend-3pg5.onrender.com/api/", methods: "GET,POST,PUT,DELETE", credentials: true }));
